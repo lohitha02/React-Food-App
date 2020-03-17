ebe900b63685cdd035e96018273ec6ed1dbf761d
Lohitha02 <lohithabhagam123@gmail.com>
2020-03-17 12:11:18 +0530

    Add: main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-class App extends Component{
-render() {
-    return(
+import './styles/style.sass';
 
-        <h1>Hello</h1>
-        );
-    }
+import Login from './pages/Login';
+import Dash from './pages/Dash';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
+function HomePage() {
+    return <Home />
 }
-export default App;
+
+function LoginPage() {
+  return <Login />;
+}
+
+function SignUpPage() {
+  return <Signup />;
+}
+
+function DashboardPage() {
+    return <Dash />;
+}
+
+function AppRoutes() {
+    return (
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        <Route exact path="/dashboard" component={DashboardPage} />
+      </Switch>
+    );
+  }
+  
+  export default function App() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Food Delivery App</title>
+        </Helmet>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </React.Fragment>
+  );
+  }
