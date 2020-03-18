2c5c9055a524d6648ee2ff2d03a61cd4f2a41c81
Lohitha02 <lohithabhagam123@gmail.com>
2020-03-18 12:53:05 +0530

    Updated with new API key

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
-  width: '100%',
+  width: '70%',
   height: '100%',
 };
 
@@ -13,12 +13,13 @@ class Maps extends React.Component {
           google={this.props.google}
           zoom={8}
           style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
+          initialCenter={{ lat: 47.444, lng: -122.176}} >
+          <Marker position={{ lat: 48.00, lng: -122.00}} />
+          </Map>
         );
     }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAx4oFC8ImmV8JBoM9ZOC3CA7PtfbE7fM4'
+    apiKey: 'AIzaSyCaK8qoLfQ8WW7M4XGe60O1_LpVrBE6yyk'
   })(Maps);
