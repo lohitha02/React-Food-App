f6bc828a009845162d6d89f4e78a44f9ab707151
Lohitha02 <lohithabhagam123@gmail.com>
2020-03-22 14:38:00 +0530

    Updated with new components

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Search from '../components/Search';
 import Maps from "../components/Maps";
 
 class Dash extends React.Component{
     render() {
         return(
+            <>
+            <Search />
             <Maps />
+            </>
         );
     }
 }
