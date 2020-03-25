617307e2f5409f53783cebe513953e26074c339d
Lohitha02 <lohithabhagam123@gmail.com>
2020-03-25 15:16:53 +0530

    Add:Cards component in the file

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import DashNav from '../components/DashNav'
+import Cards from './Cards'
 
 class Tips extends React.Component {
     render () {
         return(
             <>
-            <h1>Tips Yo!</h1>
+                <DashNav />
+                <Cards />
+
             </>
         );
     }
